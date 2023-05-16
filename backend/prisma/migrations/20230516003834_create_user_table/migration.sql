268999c06820b0f105b3874da6fc0a22085a0823
bmoki <breno26sonda@gmail.com>
2023-05-15 21:39:58 -0300

    feat: migration to add user table and .env.example file

---
+++ backend/prisma/migrations/20230516003834_create_user_table/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Service" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
