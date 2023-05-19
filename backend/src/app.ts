914a18805971179c2fd35a93f8fa899b122e0611
Breno Sonda <breno26sonda@gmail.com>
2023-05-19 16:04:26 -0300

    Backend migration (#1)
    * feat: User schema
    
    * feat: migration to add user table and .env.example file
    
    * fix:company logo
    
    * refactor: backend migrated from express to fastify

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,29 @@
-import dotenv from 'dotenv'; dotenv.config();
-import express from 'express';
-import router from './routes';
-import bodyParser from 'body-parser';
+import fastify from "fastify";
+import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
+import fastifyJwt from "@fastify/jwt";
 
-const app = express();
-const port = process.env.PORT || 3008;
+export const app = fastify();
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: "12h",
+  },
+});
+app.register(appRoutes);
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(router);
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
 
-app.listen(port, () => {
-  return console.log(`Server is running on http://localhost:${port}`);
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    //TODO: Here should log to ann external tool like DataDog/NewRelic/Sentry
+  }
+  return reply.status(500).send({ message: "Internal server error." });
 });
---
+++ backend/src/http/controllers/register.ts
@@ -0,0 +1,39 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+import { RegisterUseCase } from "@/use-cases/register";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+    avatar_url: z.string().default(""),
+    role: z.enum(["ADMIN", "MEMBER"]).default("MEMBER"),
+  });
+
+  const { role, avatar_url, email, name, password } = registerBodySchema.parse(
+    request.body
+  );
+
+  try {
+    const usersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+      avatar_url,
+      role,
+    });
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message });
+    }
+    throw err;
+  }
+
+  return reply.status(201).send();
+}
