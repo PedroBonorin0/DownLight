c7b613e018d52853642da92399d3bfd12a05c289
bmoki <breno26sonda@gmail.com>
2023-08-11 09:25:03 -0300

    test: register use case tests

---
+++ backend/src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,40 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id)
+
+    if (!user) {
+      return null
+    }
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      avatar_url: data.avatar_url,
+      role: data.role ?? "MEMBER",
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
+    return user
+  }
+}
